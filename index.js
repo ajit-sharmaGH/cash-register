abe9febeba99461380364192e025dda5b9420d4e
AJIT SHARMA <80311905+ajit-sharmaGH@users.noreply.github.com>
2021-08-29 13:46:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,48 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    hideMessage();
-
-
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-
-        } else {
-            showMessage("Do you want to do work here ??");
-
-        }
-
+  hideMessage();
+  if (billAmount.value > 0) {
+    // 12
+    if (cashGiven.value >= billAmount.value) {
+      // 2022> 12 => true
+      const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
+      calculateChange(amountToBeReturned);
     } else {
-        showMessage("Invalid bill amount");
-
+      showMessage("Do you wanna wash plates?");
     }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
 });
 
 function calculateChange(amountToBeReturned) {
-    for (let i = 0; i < availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        amountToBeReturned %= availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
+  // 2010
+  // go over all the availble
+  for (let i = 0; i < availableNotes.length; i++) {
+    // no of notes need for the denomination
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    // 2010 / 2000 = 1 || 10 / 500 = 0
 
+    // amount left after calculating the number of notes needed
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    // 2010 % 2000 = 10 || 10 % 500 = 10
+
+    // updating the no of notes in the ttble for the current amount
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
 
 function hideMessage() {
-    message.style.display = "none";
+  message.style.display = "none";
 }
 
 function showMessage(msg) {
-    console.log("here");
-
-    message.style.display = "block";
-    message.innerText = msg;
+  message.style.display = "block";
+  message.innerText = msg;
 }
